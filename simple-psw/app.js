8061c5eb26ff490ab7fcf56c6fcecb3b1681f5bb
sean diddy combs <robie.koirala@education.nsw.gov.au>
2026-03-16 14:25:54 +1100

    Update app.js

--- simple-psw/app.js
+++ simple-psw/app.js
@@ -1,1 +1,7 @@
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/dev_web/simple-pwa/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  }
 
